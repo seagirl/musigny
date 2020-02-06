58bb852783c971613c1655ab7a777c2f7428f896
Takuho Yoshizu <yoshizu@s2factory.co.jp>
2020-02-06 16:15:57 +0900

    impl init

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,41 @@
 import commander from 'commander'
+import compressing from 'compressing'
+import fs from 'fs'
+import fetch from 'node-fetch'
 import { TargetFactory } from './target.factory'
 import { Template } from './template'
 
 function main (): void {
   commander
     .version('0.0.1')
+
+  commander
+    .command('init')
+    .action(function () {
+      fetch('https://github.com/seagirl/typescript-template/archive/master.zip')
+        .then(response => {
+          const filename = 'out.zip'
+          response.body.pipe(fs.createWriteStream(filename))
+
+          if (!fs.existsSync('tmp')) {
+            fs.mkdirSync('tmp')
+          }
+
+          compressing.zip.uncompress(filename, 'tmp')
+            .then(() => {
+              fs.renameSync('tmp/typescript-template-master', 'out')
+            })
+            .catch(console.log)
+        })
+    })
+
+  commander
     .command('generate [path]')
     .action(function (path) {
       const target = TargetFactory.createFromPath(path)
 
       const template = new Template(`src/templates/${target.templatePath}`)
-      template.renderTo(`out/${target.path}.ts`, target)
+      template.renderTo(`out/src/${target.path}.ts`, target)
 
       console.log(`created: ${target}`)
     })
