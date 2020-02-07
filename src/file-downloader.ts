2f017cbc43a9ae5ed56cfccae698aea2cd663164
Takuho Yoshizu <yoshizu@s2factory.co.jp>
2020-02-07 10:48:34 +0900

    refactoring

--- src/file-downloader.ts
+++ src/file-downloader.ts
@@ -3,6 +3,8 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 
 export class FileDownloader {
+  static TMP_FILE_NAME = 'tmp'
+
   static donwloadAndExtract (url: string, rootDir: string, destination: string): void {
     if (fs.existsSync(destination)) {
       throw new Error(`destination "${destination}" is already exists`)
@@ -10,32 +12,40 @@ export class FileDownloader {
 
     fetch(url)
       .then(response => {
-        const filename = 'tmp.zip'
-        response.body.pipe(fs.createWriteStream(filename))
+        const filename = `${FileDownloader.TMP_FILE_NAME}.zip`
+        const fileStream = fs.createWriteStream(filename)
 
-        if (!fs.existsSync('tmp')) {
-          fs.mkdirSync('tmp')
+        fileStream.on('close', () => {
+          if (!fs.existsSync(FileDownloader.TMP_FILE_NAME)) {
+            fs.mkdirSync(FileDownloader.TMP_FILE_NAME)
+          }
+
+          this.extract(filename, rootDir, destination)
+        })
+
+        response.body.pipe(fileStream)
+      })
+  }
+
+  static extract (filename: string, rootDir: string, destination: string): void {
+    compressing.zip.uncompress(filename, FileDownloader.TMP_FILE_NAME)
+      .then(() => {
+        try {
+          fs.renameSync(FileDownloader.TMP_FILE_NAME + '/' + rootDir, destination)
+        } finally {
+          fs.unlinkSync(filename)
+          fs.rmdirSync(FileDownloader.TMP_FILE_NAME)
         }
+      })
+      .catch(error => {
+        console.log(error)
 
-        compressing.zip.uncompress(filename, 'tmp')
-          .then(() => {
-            try {
-              fs.renameSync('tmp/' + rootDir, destination)
-            } finally {
-              fs.unlinkSync(filename)
-              fs.rmdirSync('tmp')
-            }
-          })
-          .catch(error => {
-            console.log(error)
-
-            try {
-              fs.unlinkSync(filename)
-              fs.rmdirSync('tmp')
-            } catch (error) {
-              console.log(error)
-            }
-          })
+        try {
+          fs.unlinkSync(filename)
+          fs.rmdirSync(FileDownloader.TMP_FILE_NAME)
+        } catch (error) {
+          console.log(error)
+        }
       })
   }
 }
