f1b9c034d058c3100fd3d143ed7be06b091f10bf
seagirl <n0.sigur@gmail.com>
2020-04-15 12:48:07 +0900

    SearchOutput を標準装備

--- src/templates/app/repository/basic.repository.ts
+++ src/templates/app/repository/basic.repository.ts
@@ -6,8 +6,13 @@ export interface SearchInput {
   offset?: number;
 }
 
+export interface SearchOutput {
+  entities: MusignyEntityNameBasicEntity[];
+  hasNext: boolean;
+}
+
 export interface MusignyEntityNameBasicRepository extends IdentifierGenerator<number> {
-  search(input?: SearchInput): Promise<MusignyEntityNameBasicEntity[]>;
+  search(input?: SearchInput): Promise<SearchOutput>;
   find(id: number): Promise<MusignyEntityNameBasicEntity | undefined>;
   save(entity: MusignyEntityNameBasicEntity): Promise<void>;
   delete(entity: MusignyEntityNameBasicEntity): Promise<void>;
@@ -15,7 +20,7 @@ export interface MusignyEntityNameBasicRepository extends IdentifierGenerator<nu
 
 export class MockMusignyEntityNameBasicRepository implements MusignyEntityNameBasicRepository {
   nextIdentifier (): Promise<number> { return Promise.resolve(0) }
-  search (): Promise<MusignyEntityNameBasicEntity[]> { return Promise.resolve([]) }
+  search (): Promise<SearchOutput> { return Promise.resolve({ entities: [], hasNext: false }) }
   find (): Promise<MusignyEntityNameBasicEntity | undefined> { return Promise.resolve(undefined) }
   save (): Promise<void> { return Promise.resolve() }
   delete (): Promise<void> { return Promise.resolve() }
